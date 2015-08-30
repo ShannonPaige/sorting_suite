f302726c9a53ab24ac42817d67c700159e764e1a
ShannonPaige <spaige@gmail.com>
2015-08-30 10:14:38 -0600

    working on merge method

--- lib/merge.rb
+++ lib/merge.rb
@@ -1,23 +1,18 @@
 def merge(left_half, right_half)
-  sorted = []                     # => []
-  ll = left_half.length           # => 2
-  rl = right_half.length          # => 2
-  i, j, k = 1,1,1                 # => [1, 1, 1]
-
-  while i < left_half.length && j < right_half.length  # => true, false
-    if left_half[i] < right_half[j]                    # => true
-      sorted.push(left_half.shift)                     # => ["b"]
-      i += 1                                           # => 2
+  sorted = []
+  while left_half.length != 0 && right_half.length !=0
+    if left_half[i] < right_half[j]
+      sorted.push(left_half.shift)
     else
       sorted.push(right_half.shift)
-      j +=1
-    end                                                # => 2
-  end                                                  # => nil
-  # if left_half.length == 0
-  #   sorted.push(right_half.shift)
-  # elsif right_half.length == 0
-  #   sorted.push(left_half.shift)                               # => nil
-  sorted                                               # => ["b"]
-end                                                    # => :merge
+    end
+  end
+  if left_half.length == 0
+    sorted.push(right_half.shift) #need to push the whole array in, not just one element
+  else right_half.length == 0
+    sorted.push(left_half.shift)
+  end
+  sorted
+end
 
-merge(["b","d"],["z","k"])  # => ["b"]
+merge(["b","d"],["z","k"])
