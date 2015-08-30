a9e569afefed8e15797b17ec4b5967e2e106fd90
ShannonPaige <spaige@gmail.com>
2015-08-30 14:44:05 -0600

    was totally wrong before, but this might be it

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -3,73 +3,79 @@ module SortingSuite
     attr_accessor :unsorted  # => nil
 
     def initialize(unsorted)
-      @unsorted = unsorted    # => [3, 4, 2]
+      @unsorted = unsorted    # => [1, 2, 3, 9, 7, 6]
     end                       # => :initialize
 
     def sort
-      merge_split(unsorted)  # => [2, 3, 4]
+      merge_split(unsorted)  # => [1, 2, 3, 6, 7, 9]
     end                      # => :sort
 
     def merge_split(unsorted)
-      size = unsorted.length                                        # => 3, 1, 2, 1, 1
-      midpoint = size/2                                             # => 1, 0, 1, 0, 0
-      # half_array_size = size/2.to_f                     # => 1.5, 0.5, 1.0, 0.5, 0.5
-      # l = half_array_size.floor                         # => 1, 0, 1, 0, 0
-      # r = size - l                                      # => 2, 1, 1, 1, 1
-      if size <= 1                                                  # => false, true, false, true, true
-        unsorted                                                    # => [3], [4], [2]
-      else
-        unsorted.slice(0, midpoint)                                 # => [3], [4]
-        unsorted.slice(midpoint, size-1)                            # => [4, 2], [2]
-        left_half = merge_split(unsorted.slice(0, midpoint))        # => [3], [4]
-        right_half = merge_split(unsorted.slice(midpoint, size-1))  # => [2], [2, 4]
-        merge(left_half, right_half)                                # => [2, 4], [2, 3, 4]
-      end                                                           # => [3], [4], [2], [2, 4], [2, 3, 4]
-    end                                                             # => :merge_split
+      size = unsorted.length   # => 6, 3, 1, 2, 1, 1, 3, 1, 2, 1, 1
+      midpoint = size/2        # => 3, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0
 
-    def merge(left_half, right_half)
-      sorted=[]                       # => [], []
-      left_half                       # => [4], [3]
-      right_half                      # => [2], [2, 4]
-      ll = left_half.length           # => 1, 1
-      rl = right_half.length          # => 1, 2
-      i, j, k = 0,0,0                 # => [0, 0, 0], [0, 0, 0]
+      #base case
+      return unsorted if size <= 1  # => false, false, true, false, true, true, false, true, false, true, true
 
-      while i < left_half.length && j < right_half.length        # => true, false, true, true, false
-        right_half                                               # => [2], [2, 4], [2, 4]
-        if left_half[i] < right_half[j]                          # => false, false, true
-                    right_half                                   # => [2, 4]
-          sorted[k]=left_half[i]                                 # => 3
-                    right_half                                   # => [2, 4]
-          i += 1                                                 # => 1
-                    right_half                                   # => [2, 4]
-          left_half                                              # => [3]
-          right_half                                             # => [2, 4]
+      #general pattern
+      unsorted.slice(0, midpoint)                                       # => [1, 2, 3], [1], [2], [9], [7]
+      unsorted.slice(midpoint, size-midpoint)                           # => [9, 7, 6], [2, 3], [3], [7, 6], [6]
+      left_half = merge_split unsorted.slice(0, midpoint)               # => [1], [2], [1, 2, 3], [9], [7]
+      right_half = merge_split unsorted.slice(midpoint, size-midpoint)  # => [3], [2, 3], [6], [6, 7], [6, 7, 9]
+
+      # merge(left_half, right_half)
+      sorted = []      # => [], [], [], [], []
+      i, j, k = 0,0,0  # => [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]
+
+      while i < left_half.length && j < right_half.length             # => true, false, true, false, true, false, true, true, false, true, true, true, false
+        left_half[i]                                                  # => 2, 1, 7, 9, 9, 1, 2, 3
+        right_half[j]                                                 # => 3, 2, 6, 6, 7, 6, 6, 6
+        if left_half[i] < right_half[j]                               # => true, true, false, false, false, true, true, true
+          sorted[k]=left_half[i]                                      # => 2, 1, 1, 2, 3
+          i += 1                                                      # => 1, 1, 1, 2, 3
         else
-          sorted[k]=right_half[j]                                # => 2, 2
-          sorted                                                 # => [2], [2]
-          j +=1                                                  # => 1, 1
-          left_half                                              # => [4], [3]
-          right_half                                             # => [2], [2, 4]
-        end                                                      # => [2], [2, 4], [2, 4]
-        k +=1                                                    # => 1, 1, 2
-        left_half                                                # => [4], [3], [3]
-        right_half                                               # => [2], [2, 4], [2, 4]
-      end                                                        # => nil, nil
-      if i >= left_half.length                                   # => false, true
-        # take right[j] thru end and push into sorted
-        sorted                                                   # => [2, 3]
-        right_half                                               # => [2, 4]
-        right_half[1]                                            # => 4
-        left_half                                                # => [3]
-        right_half[1]                                            # => 4
-        sorted.push(right_half.slice(j, rl-1)).flatten!          # => [2, 3, 4]
+          sorted[k]=right_half[j]                                     # => 6, 6, 7
+          sorted                                                      # => [6], [6], [6, 7]
+          j +=1                                                       # => 1, 1, 2
+        end                                                           # => 1, 1, 1, 1, 2, 1, 2, 3
+        k +=1                                                         # => 1, 1, 1, 1, 2, 1, 2, 3
+      end                                                             # => nil, nil, nil, nil, nil
+      if i >= left_half.length                                        # => true, true, false, false, true
+        sorted.push(right_half.slice(j, right_half.length)).flatten!  # => [2, 3], [1, 2, 3], [1, 2, 3, 6, 7, 9]
       else
-        sorted.push(left_half.shift(left_half.length)).flatten!  # => [2, 4]
-      end                                                        # => [2, 4], [2, 3, 4]
-      sorted                                                     # => [2, 4], [2, 3, 4]
-    end                                                          # => :merge
+        sorted.push(left_half.shift(left_half.length)).flatten!       # => [6, 7], [6, 7, 9]
+      end                                                             # => [2, 3], [1, 2, 3], [6, 7], [6, 7, 9], [1, 2, 3, 6, 7, 9]
+      sorted                                                          # => [2, 3], [1, 2, 3], [6, 7], [6, 7, 9], [1, 2, 3, 6, 7, 9]
+    end                                                               # => :merge_split
 
-  end                                      # => :merge
-end                                        # => :merge
-SortingSuite::MergeSort.new([3,4,2]).sort  # => [2, 3, 4]
+    # def merge(left_half, right_half)
+    #   left_half
+    #   right_half
+    #   sorted = []
+    #   ll = left_half.length
+    #   rl = right_half.length
+    #   i, j, k = 0,0,0
+    #
+    #   while i < left_half.length && j < right_half.length
+    #     if left_half[i] < right_half[j]
+    #       sorted[k]=left_half[i]
+    #       i += 1
+    #     else
+    #       sorted[k]=right_half[j]
+    #       sorted
+    #       j +=1
+    #     end
+    #     k +=1
+    #   end
+    #   if i >= left_half.length
+    #     sorted
+    #     sorted.push(right_half.slice(j, rl-1)).flatten!
+    #   else
+    #     sorted.push(left_half.shift(left_half.length)).flatten!
+    #   end
+    #   sorted
+    # end                                                          # => :merge
+
+  end                                            # => :merge_split
+end                                              # => :merge_split
+SortingSuite::MergeSort.new([1,2,3,9,7,6]).sort  # => [1, 2, 3, 6, 7, 9]
