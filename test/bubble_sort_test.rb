dec752312ef6bbbbc66ed55174ca0be15af10533
ShannonPaige <spaige@gmail.com>
2015-08-30 16:33:45 -0600

    added more tests

--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -2,8 +2,12 @@ require "bubble_sort"
 
 class SortingSuite::BubbleSortTest < MiniTest::Test
 
-  def test_empty_array_sorts
+  def test_sorts_empty_arrays
     assert_equal [], SortingSuite::BubbleSort.new([]).sort
+  end
+
+  def test_sorts_one_element_arrays
+    assert_equal [5], SortingSuite::BubbleSort.new([5]).sort
   end
 
   def test_sorts_an_array_of_one_letter_strings
@@ -16,5 +20,13 @@ class SortingSuite::BubbleSortTest < MiniTest::Test
 
   def test_sorts_a_large_array_of_numbers
     assert_equal [1, 2, 3, 4, 5], SortingSuite::BubbleSort.new([4, 2, 5, 3, 1]).sort
+  end
+
+  def test_sorts_arrays_that_are_in_reverse
+    assert_equal [1, 2, 3, 4, 5], SortingSuite::BubbleSort.new([5, 4, 3, 2, 1]).sort
+  end
+
+  def test_sorts_arrays_that_are_already_in_order
+    assert_equal [1, 2, 3, 4, 5], SortingSuite::BubbleSort.new([1, 2, 3, 4, 5]).sort
   end
 end
--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -2,15 +2,31 @@ require "merge_sort"
 
 class SortingSuite::MergeSortTest < MiniTest::Test
 
-  def test_empty_array_sorts
+  def test_sorts_empty_arrays
     assert_equal [], SortingSuite::MergeSort.new([]).sort
   end
 
+  def test_sorts_one_element_arrays
+    assert_equal [5], SortingSuite::MergeSort.new([5]).sort
+  end
+
+  def test_sorts_an_array_of_one_letter_strings
+    assert_equal ["a", "b", "c", "d"], SortingSuite::MergeSort.new(["d", "b" ,"a" ,"c"]).sort
+  end
+
   def test_sorts_a_small_array_of_numbers
-    assert_equal [1,2,3], SortingSuite::MergeSort.new([3,2,1]).sort
+    assert_equal [1, 2, 3], SortingSuite::MergeSort.new([3, 2, 1]).sort
   end
 
   def test_sorts_a_large_array_of_numbers
-    assert_equal [1,2,3,4,5], SortingSuite::MergeSort.new([5,3,1,2,4]).sort
+    assert_equal [1, 2, 3, 4, 5], SortingSuite::MergeSort.new([4, 2, 5, 3, 1]).sort
+  end
+
+  def test_sorts_arrays_that_are_in_reverse
+    assert_equal [1, 2, 3, 4, 5], SortingSuite::MergeSort.new([5, 4, 3, 2, 1]).sort
+  end
+
+  def test_sorts_arrays_that_are_already_in_order
+    assert_equal [1, 2, 3, 4, 5], SortingSuite::MergeSort.new([1, 2, 3, 4, 5]).sort
   end
 end
