601565645f30492164ca996b22c196d33fc3d0c9
ShannonPaige <spaige@gmail.com>
2015-08-30 20:27:50 -0600

    iinsertion sort with in-place in place, although its not working yet

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,10 +1,10 @@
 module SortingSuite
   class InsertionSort
-    attr_accessor :unsorted
+    attr_accessor :unsorted  # => nil
 
     def initialize(unsorted)
-      @unsorted = unsorted
-    end
+      @unsorted = unsorted    # => [3, 2, 1]
+    end                       # => :initialize
 
     def sort
       pass = 0
@@ -28,6 +28,30 @@ module SortingSuite
         pass += 1
       end
       sorted
-    end
-  end
-end
+    end                                   # => :sort
+
+    def inplace_sort                # ~> ArgumentError: wrong number of arguments (0 for 1)
+      unsorted.each_index do |i|
+        if i == 0
+          number = unsorted[i]
+        else
+          index = 0
+          while index < i
+            if unsorted[i] < unsorted[index]
+              number = unsorted.delete_at(i)
+              unsorted.insert(index, number)
+            end
+          end
+        end
+      end
+    end  # => :inplace_sort
+  end    # => :inplace_sort
+end      # => :inplace_sort
+
+SortingSuite::InsertionSort.new([3, 2, 1]).inplace_sort
+
+# ~> ArgumentError
+# ~> wrong number of arguments (0 for 1)
+# ~>
+# ~> /Users/shannonpaige/code/sorting-suite/lib/insertion_sort.rb:33:in `inplace_sort'
+# ~> /Users/shannonpaige/code/sorting-suite/lib/insertion_sort.rb:74:in `<main>'
